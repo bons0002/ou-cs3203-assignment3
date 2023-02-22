6ba31e8094bb9012e1cdcea79bae72cb5c1020f1
bons0002 <sam_bons@yahoo.com>
2023-02-21 21:21:27 -0600

    Implement function to compute product of a list

--- hw3.cpp
+++ hw3.cpp
@@ -11,3 +11,12 @@ float compute_sum(vector<float> list)
 
     return sum;
 }
+
+// Compute the product of a list of numbers
+float compute_prod(vector<float> list)
+{
+    float prod = 1;
+    for (int i = 0; i < list.size(); i++)   {prod *= list[i];}
+
+    return prod;
+}
